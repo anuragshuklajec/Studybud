9eabe7d5a48db767311a40257bb241439f6ca49c
anuragshuklajec <anuragshuklajec@gmail.com>
2022-09-26 20:18:07 +0530

    create of crud done

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 def home(request):
@@ -11,3 +12,13 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form':form}
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'base/room_form.html',context)
