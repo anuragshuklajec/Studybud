7c69a4fe4b7616ea705c6ee9a0c1d7ede530dda6
anuragshuklajec <anuragshuklajec@gmail.com>
2022-09-27 19:03:02 +0530

    add room_count

--- base/views.py
+++ base/views.py
@@ -13,7 +13,8 @@ def home(request):
     
     )
     topics = Topic.objects.all()
-    context = {'rooms':rooms,'topics':topics}
+    room_count = rooms.count()
+    context = {'rooms':rooms,'topics':topics,'room_count':room_count}
     return render(request,'base/home.html',context) 
 
 def room(request,pk):
